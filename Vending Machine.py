d1112bca99d3f0c740d4ea667650ba0bf8900b11
shaheer60 <145544187+shaheer60@users.noreply.github.com>
2024-01-25 10:09:00 +0400

    Create Vending Machine.py

---
+++ Vending Machine.py
@@ -0,0 +1,122 @@
+
+print('''──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+─██████████████─██████──██████─██████████████────██████──██████─██████████████─██████──────────██████─████████████───██████████─██████──────────██████─██████████████─
+─██░░░░░░░░░░██─██░░██──██░░██─██░░░░░░░░░░██────██░░██──██░░██─██░░░░░░░░░░██─██░░██████████──██░░██─██░░░░░░░░████─██░░░░░░██─██░░██████████──██░░██─██░░░░░░░░░░██─
+─██████░░██████─██░░██──██░░██─██░░██████████────██░░██──██░░██─██░░██████████─██░░░░░░░░░░██──██░░██─██░░████░░░░██─████░░████─██░░░░░░░░░░██──██░░██─██░░██████████─
+─────██░░██─────██░░██──██░░██─██░░██────────────██░░██──██░░██─██░░██─────────██░░██████░░██──██░░██─██░░██──██░░██───██░░██───██░░██████░░██──██░░██─██░░██─────────
+─────██░░██─────██░░██████░░██─██░░██████████────██░░██──██░░██─██░░██████████─██░░██──██░░██──██░░██─██░░██──██░░██───██░░██───██░░██──██░░██──██░░██─██░░██─────────
+─────██░░██─────██░░░░░░░░░░██─██░░░░░░░░░░██────██░░██──██░░██─██░░░░░░░░░░██─██░░██──██░░██──██░░██─██░░██──██░░██───██░░██───██░░██──██░░██──██░░██─██░░██──██████─
+─────██░░██─────██░░██████░░██─██░░██████████────██░░██──██░░██─██░░██████████─██░░██──██░░██──██░░██─██░░██──██░░██───██░░██───██░░██──██░░██──██░░██─██░░██──██░░██─
+─────██░░██─────██░░██──██░░██─██░░██────────────██░░░░██░░░░██─██░░██─────────██░░██──██░░██████░░██─██░░██──██░░██───██░░██───██░░██──██░░██████░░██─██░░██──██░░██─
+─────██░░██─────██░░██──██░░██─██░░██████████────████░░░░░░████─██░░██████████─██░░██──██░░░░░░░░░░██─██░░████░░░░██─████░░████─██░░██──██░░░░░░░░░░██─██░░██████░░██─
+─────██░░██─────██░░██──██░░██─██░░░░░░░░░░██──────████░░████───██░░░░░░░░░░██─██░░██──██████████░░██─██░░░░░░░░████─██░░░░░░██─██░░██──██████████░░██─██░░░░░░░░░░██─
+─────██████─────██████──██████─██████████████────────██████─────██████████████─██████──────────██████─████████████───██████████─██████──────────██████─██████████████─
+──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+─██████──────────██████─██████████████─██████████████─██████──██████─██████████─██████──────────██████─██████████████─
+─██░░██████████████░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░██──██░░██─██░░░░░░██─██░░██████████──██░░██─██░░░░░░░░░░██─
+─██░░░░░░░░░░░░░░░░░░██─██░░██████░░██─██░░██████████─██░░██──██░░██─████░░████─██░░░░░░░░░░██──██░░██─██░░██████████─
+─██░░██████░░██████░░██─██░░██──██░░██─██░░██─────────██░░██──██░░██───██░░██───██░░██████░░██──██░░██─██░░██─────────
+─██░░██──██░░██──██░░██─██░░██████░░██─██░░██─────────██░░██████░░██───██░░██───██░░██──██░░██──██░░██─██░░██████████─
+─██░░██──██░░██──██░░██─██░░░░░░░░░░██─██░░██─────────██░░░░░░░░░░██───██░░██───██░░██──██░░██──██░░██─██░░░░░░░░░░██─
+─██░░██──██████──██░░██─██░░██████░░██─██░░██─────────██░░██████░░██───██░░██───██░░██──██░░██──██░░██─██░░██████████─
+─██░░██──────────██░░██─██░░██──██░░██─██░░██─────────██░░██──██░░██───██░░██───██░░██──██░░██████░░██─██░░██─────────
+─██░░██──────────██░░██─██░░██──██░░██─██░░██████████─██░░██──██░░██─████░░████─██░░██──██░░░░░░░░░░██─██░░██████████─
+─██░░██──────────██░░██─██░░██──██░░██─██░░░░░░░░░░██─██░░██──██░░██─██░░░░░░██─██░░██──██████████░░██─██░░░░░░░░░░██─
+─██████──────────██████─██████──██████─██████████████─██████──██████─██████████─██████──────────██████─██████████████─
+──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────''')
+
+# Defining a list of dictionaries representing each product in the vending machine
+item_1 = {'Item Code': '01', 'Item Name': 'Water', 'Item price': 1, 'item stock': 3}
+item_2 = {'Item Code': '02', 'Item Name': 'pepsi', 'Item price': 3, 'item stock': 6}
+item_3 = {'Item Code': '03', 'Item Name': 'Redbull', 'Item price': 6.5, 'item stock': 1}
+item_4 = {'Item Code': '04', 'Item Name': 'Fanta', 'Item price': 2.5, 'item stock': 8}
+item_5 = {'Item Code': '05', 'Item Name': 'Croissant', 'Item price': 2.5, 'item stock': 6}
+item_6 = {'Item Code': '06', 'Item Name': 'Cheetos', 'Item price': 3.5, 'item stock': 6}
+item_7 = {'Item Code': '07', 'Item Name': 'Kit-Kat', 'Item price': 1.5, 'item stock': 3}
+item_8 = {'Item Code': '08', 'Item Name': 'Milkyway', 'Item price': 3, 'item stock': 0}
+item_9 = {'Item Code': '09', 'Item Name': 'Mars', 'Item price': 3, 'item stock': 4}
+item_10 = {'Item Code': '10', 'Item Name': 'Galaxy', 'Item price': 3, 'item stock': 9}
+item_11 = {'Item Code': '11', 'Item Name': 'Skittles', 'Item price': 3, 'item stock': 2}
+item_12 = {'Item Code': '12', 'Item Name': 'Snickers', 'Item price': 3.5, 'item stock': 2}
+
+# Creating a list called products to store all the defined items.
+products = [item_1, item_2, item_3, item_4, item_5, item_6, item_7, item_8, item_9, item_10, item_11, item_12]
+
+# Initializing the user's wallet and a list to store transaction history
+wallet = 0
+transaction_history = []
+
+print('----------WELCOME TO MY VENDING MACHINE-----------')
+
+# A Continuous loop for the vending machine operations
+while True:
+    # Displaying the list of available products along with their details
+    for item in products:
+        print(f"{item['Item Code']}     \t  \t{item['Item Name']}    \t  \t {item['Item price']}      \tStock: {item['item stock']}")
+
+    # Asking the user to input the amount of money they want to add
+    try:
+        money_am = float(input("\nEnter the amount of money (0 to exit): "))
+
+    # If an invalid input is provided it asks again
+    except ValueError:
+        print("Invalid input. Please enter a valid numeric value.")
+        continue
+
+    # If the user enters 0 it exits the vending machine
+    if money_am == 0:
+        break
+
+    # Adding the entered money to the user's wallet to show their balance
+    wallet += money_am
+
+    # Asking the user to input the product code they want to purchase
+    pd_code = input("Enter the product code: ")
+
+    # Searching for the selected item in the products list
+    selected_item = next((item for item in products if item['Item Code'] == pd_code), None)
+
+    if selected_item:
+        # Checking if the selected item is in stock
+        if selected_item['item stock'] > 0:
+            # Checking if the user has enough money to purchase the item
+            if wallet >= selected_item['Item price']:
+                # Calculating the change and dispensing the item
+                change = wallet - selected_item['Item price']
+                print(f"\nDispensing {selected_item['Item Name']}")
+                print("Your change is", change)
+
+                # Updating the item stock and recording the transaction also updating the wallet
+                selected_item['item stock'] -= 1
+                transaction_history.append(
+                    {'Item Name': selected_item['Item Name'], 'Amount Spent': selected_item['Item price'],
+                     'Change Given': change})
+                wallet = change
+                print(f"Updated Wallet Balance: {wallet}\n")
+
+                # Asking if the user wants to make another purchase
+                additional_purchase = input("Do you want to make another purchase? (yes/no): ").lower()
+                if additional_purchase != 'yes':
+                    break  # Exits the loop if the user doesn't want to make another purchase
+            else:
+                # Informing the user if their wallet has insufficient funds
+                remain = selected_item['Item price'] - wallet
+                print(f"Cash insufficient. Amount pending: {remain}")
+        else:
+            # Informing the user if the selected item is out of stock
+            print("The item is out of stock.")
+    else:
+        # Printing invalid code when it does not find a matching product
+        print("Invalid product code. Please enter a valid code.")
+
+# Displaying the transaction history at the end
+print("\n-------- TRANSACTION HISTORY --------")
+for transaction in transaction_history:
+    print(f"Item: {transaction['Item Name']}, Amount Spent: {transaction['Amount Spent']}, Change Given: {transaction['Change Given']}")
+
+# Displaying the final wallet balance
+print(f"\nFinal Wallet Balance: {wallet}")
+
+# Thanking the user for using the vending machine
+print("!!! Thank you for using my vending machine. Have a great day !!!")
